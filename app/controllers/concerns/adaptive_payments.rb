fd10a8804b5d312f4f671f1b3557ee9967a0487e
nuderand <nuderebatsin.roal@gmail.com>
2016-03-30 01:24:29 +0900

    add preapproval service #13

---
+++ app/controllers/concerns/adaptive_payments.rb
@@ -0,0 +1,45 @@
+module AdaptivePayments
+  def adaptive_payments_api
+    @api ||= PayPal::SDK::AdaptivePayments::API.new
+  end
+
+  private
+
+  def preapproval_options(opts = {})
+    preapproval_options = { :currencyCode => 'USD',
+                            :maxTotalAmountOfAllPayments => 1.00,
+                            :displayMaxTotalAmount => TRUE,
+                            :startingDate => Time.now,
+                            :endingDate => Time.now.months_since(1), # opts[:endingDate]
+                            :maxNumberOfPayments => 1,
+                            :maxNumberOfPaymentsPerPeriod => 1,
+                            :pinType => 'NOT_REQUIRED',
+                            :requestEnvelope => 'en_US',
+                            :feesPayer => 'SENDER',
+                            # :senderEmail => opts[:email],
+                            :requestEnvelope => {
+                              :errorLanguage => 'en_US' },
+                            :cancelUrl => application_url(cancel_pledges_path),
+                            :returnUrl => application_url(complete_pledges_path) }
+  end
+
+  def self.api_base_url
+    if Rails.env.production?
+      'https://www.paypal.com'
+    else
+      'https://www.sandbox.paypal.com'
+    end
+  end
+
+  def preapproval_url(key)
+    "#{AdaptivePayments.api_base_url}/cgi-bin/webscr?cmd=_ap-preapproval&preapprovalkey=#{key}"
+  end
+
+  def application_url(path)
+    if params[:locale].nil?
+      "#{root_url.chop}#{path}"
+    else
+      root_url[-1, 1] == '/' ? "#{root_url}#{params[:locale]}#{path}" : "#{root_url.gsub(root_path, '')}#{path}"
+    end
+  end
+end
--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -3,6 +3,8 @@ class PledgesController < ApplicationController
   before_action :set_pledge, only: [:show, :edit, :update, :destroy]
   before_action :require_login, except: [:index, :show]
 
+  include AdaptivePayments
+
   # GET /pledges
   def index
     @pledges = Pledge.all
@@ -34,7 +36,14 @@ class PledgesController < ApplicationController
     @pledge.pledged_at = Time.zone.now
 
     if @pledge.save
-      redirect_to @pledge, notice: 'Pledge was successfully created.'
+      preapproval = adaptive_payments_api.build_preapproval(preapproval_options)
+      preapproval_response = adaptive_payments_api.preapproval(preapproval)
+
+      if preapproval_response.success?
+        redirect_to preapproval_url(preapproval_response.preapprovalKey)
+      else
+        render :new
+      end
     else
       render :new
     end
