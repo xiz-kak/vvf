7afe65b77476f35d1710aeba45ffca3f6460d8ce
xiz <shizuka.kakimoto@linkage.jp.net>
2016-05-25 19:41:03 +0900

    skip rspec

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe UserMailer, type: :mailer do
   describe "activation_needed_email" do
     let(:mail) { UserMailer.activation_needed_email }
 
-    it "renders the headers" do
+    xit "renders the headers" do
       expect(mail.subject).to eq("Activation needed email")
       expect(mail.to).to eq(["to@example.org"])
       expect(mail.from).to eq(["from@example.com"])
     end
 
-    it "renders the body" do
+    xit "renders the body" do
       expect(mail.body.encoded).to match("Hi")
     end
   end
@@ -18,13 +18,13 @@ RSpec.describe UserMailer, type: :mailer do
   describe "activation_success_email" do
     let(:mail) { UserMailer.activation_success_email }
 
-    it "renders the headers" do
+    xit "renders the headers" do
       expect(mail.subject).to eq("Activation success email")
       expect(mail.to).to eq(["to@example.org"])
       expect(mail.from).to eq(["from@example.com"])
     end
 
-    it "renders the body" do
+    xit "renders the body" do
       expect(mail.body.encoded).to match("Hi")
     end
   end
