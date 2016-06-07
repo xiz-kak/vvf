94e1804e6267ca0d6b781fae5474ea2e4854c18e
xiz <shizuka.kakimoto@linkage.jp.net>
2016-06-07 18:54:41 +0900

    multi langauge for emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,8 +8,8 @@ class UserMailer < ApplicationMailer
   def activation_needed_email(user)
     @user = user
     @url  = "#{ENV['APP_URL']}/#{locale}/users/#{user.activation_token}/activate"
-    mail(:to => user.email,
-         :subject => "[VinVin-Funding] Welcome to Vin-Vin Funding")
+    mail(to: user.email,
+         subject: t('mail_subject.activation_needed'))
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -20,14 +20,14 @@ class UserMailer < ApplicationMailer
   def activation_success_email(user)
     @user = user
     @url  = "#{ENV['APP_URL']}/#{locale}/login"
-    mail(:to => user.email,
-         :subject => "[VinVin-Funding] Your account is now activated")
+    mail(to: user.email,
+         subject: t('mail_subject.activation_success'))
   end
 
   def reset_password_email(user)
     @user = User.find user.id
     @url  = "#{ENV['APP_URL']}/#{locale}#{edit_password_reset_url(id: @user.reset_password_token, only_path: true)}"
-    mail(:to => user.email,
-         :subject => "[VinVin-Funding] Your password has been reset")
+    mail(to: user.email,
+         subject: t('mail_subject.reset_password'))
   end
 end
